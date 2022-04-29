6aef7f655535c39ee737ce1990a7015493c75866
og <ocikemn@gmail.com>
2022-04-29 16:15:05 +0300

    master

--- lab5_src/top.v
+++ lab5_src/top.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
-module top(clk, rst, direction, sseg, anode, mode, select_1, select_2, select_3);
+module top(clk, rst, direction, sseg, anode, mode, btn);
 
-input clk, rst, direction, mode, select;
+input clk, rst, direction, mode, btn;
 output [7:0] sseg;
 output [3:0] anode;
 
@@ -11,23 +11,28 @@ reg [4:0] addr_1,addr_2,addr_3,addr_4;
 wire [7:0] sseg_4,sseg_3,sseg_2,sseg_1;
 
 reg [3:0] state_current, state_next;
+reg [4:0] addr_1_current, addr_1_next, addr_2_current, addr_2_next, addr_3_current, addr_3_next;
 
-addr_1 = 0;
-addr_2 = 0;
-addr_3 = 0;
-addr_4 = 0;
+
 
 always@(posedge clk) begin
     if(rst) begin
         state_current <= 0;
+        addr_1_current <= 0;
+        addr_2_current <= 0;
+        addr_3_current <= 0;
     end
     else begin
         state_current <= state_next;
+        addr_1_current <= addr_1_next;
+        addr_2_current <= addr_2_next;
+        addr_3_current <= addr_3_next;
     end
 end
 
 always@(*) begin
     state_next = state_current;
+    addr_1_next = addr_1_current;
     
     if (mode == 1) begin
       case(state_current)
@@ -36,6 +41,7 @@ always@(*) begin
                 addr_2 = 16; // g
                 addr_3 = 27; // u
                 addr_4 = 29; // z
+
                 if (enable == 1 && direction == 1) begin
                   state_next = 1;
                 end else if (enable == 1 && direction == 0) begin
@@ -43,10 +49,11 @@ always@(*) begin
                 end
             end
             1:begin
-                addr_1 = 16; // g
-                addr_2 = 27; // u
-                addr_3 = 29; // z
-                addr_4 = 22; // o
+                addr_1 = 0;  // 0
+                addr_2 = 22; // o
+                addr_3 = 16; // g
+                addr_4 = 27; // u
+                
                 if (enable == 1 && direction == 1) begin
                   state_next = 2;
                 end else if (enable == 1 && direction == 0) begin
@@ -54,10 +61,11 @@ always@(*) begin
                 end
             end
             2:begin
-                addr_1 = 27; // u
-                addr_2 = 29; // z
+                addr_1 = 29; // z
+                addr_2 = 0;  // 0
                 addr_3 = 22; // o
                 addr_4 = 16; // g
+                
                 if (enable == 1 && direction == 1) begin
                   state_next = 3;
                 end else if (enable == 1 && direction == 0) begin
@@ -65,222 +73,50 @@ always@(*) begin
                 end
             end
             3:begin
-                addr_1 = 29; // z
-                addr_2 = 22; // o
-                addr_3 = 16; // g
-                addr_4 = 27; // u
+                addr_1 = 27; // u
+                addr_2 = 29; // z
+                addr_3 = 0;  // 0
+                addr_4 = 22; // o
+                
                 if (enable == 1 && direction == 1) begin
-                  state_next = 0;
+                  state_next = 4;
                 end else if (enable == 1 && direction == 0) begin
                   state_next = 2;
                 end
             end
+            4:begin
+                addr_1 = 16; // g
+                addr_2 = 27; // u
+                addr_3 = 29; // z
+                addr_4 = 0;  // 0
+                
+                if (enable == 1 && direction == 1) begin
+                  state_next = 0;
+                end else if (enable == 1 && direction == 0) begin
+                  state_next = 3;
+                end
+            end
       endcase
     end else if (mode == 0) begin
-        case (state_current)
+          case (state_current)
 
-
-          0:begin
-              addr_1 = 0;
-              if (select == 0) begin
+            0:begin
+                addr_1_next = (addr_1_current + 1) % 10;
+                addr_1 = addr_1_next;
+                if (btn == 1) begin
                   state_next = 1;
-              end else if (select == 1) begin
-                  state_next = 11;
+                end               
               end
-            end
-          1:begin
-            addr_1 = 1;
-              if (select == 0) begin
+
+            1:begin
+                addr_2_next = (addr_2_current + 1) % 10;
+                addr_2 = addr_1_next;
+                if (btn == 1) begin
                   state_next = 2;
-              end else if (select == 1) begin
-                  state_next = 11;
+                end               
               end
-            end
-          2:begin
-            addr_1 = 2;
-              if (select == 0) begin
-                  state_next = 3;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          3:begin
-            addr_1 = 3;
-              if (select == 0) begin
-                  state_next = 4;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          4:begin
-            addr_1 = 4;
-              if (select == 0) begin
-                  state_next = 5;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          5:begin
-            addr_1 = 5;
-              if (select == 0) begin
-                  state_next = 6;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          6:begin
-            addr_1 = 6;
-              if (select == 0) begin
-                  state_next = 7;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          7:begin
-            addr_1 = 7;
-              if (select == 0) begin
-                  state_next = 8;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          8:begin
-            addr_1 = 8;
-              if (select == 0) begin
-                  state_next = 9;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          9:begin
-            addr_1 = 9;
-              if (select == 0) begin
-                  state_next = 0;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
 
-
-          11:begin
-              addr_2 = 10;
-              if (select == 0) begin
-                
-                state_next = 12;
-              end
-            end
-          12:begin
-              if (select == 0) begin
-                addr_2 = 25;
-                state_next = 13;
-              end
-            end
-          13:begin
-              if (select == 0) begin
-                addr_2 = 13;
-                state_next = 14;
-              end
-            end
-          14:begin
-              if (select == 0) begin
-                addr_2 = 21;
-                state_next = 11;
-              end
-            end
-
-
-          20:begin
-              addr_3 = 0;
-              if (select == 0) begin
-                  state_next = 21;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          21:begin
-            addr_3 = 1;
-              if (select == 0) begin
-                  state_next = 22;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          22:begin
-            addr_3 = 2;
-              if (select == 0) begin
-                  state_next = 23;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          23:begin
-            addr_3 = 3;
-              if (select == 0) begin
-                  state_next = 24;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          24:begin
-            addr_3 = 4;
-              if (select == 0) begin
-                  state_next = 25;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          25:begin
-            addr_3 = 5;
-              if (select == 0) begin
-                  state_next = 26;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          26:begin
-            addr_3 = 6;
-              if (select == 0) begin
-                  state_next = 27;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          27:begin
-            addr_3 = 7;
-              if (select == 0) begin
-                  state_next = 28;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          28:begin
-            addr_3 = 8;
-              if (select == 0) begin
-                  state_next = 29;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          29:begin
-            addr_3 = 9;
-              if (select == 0) begin
-                  state_next = 20;
-              end else if (select == 1) begin
-                  state_next = 11;
-              end
-            end
-          30:begin
-            if (addr_2 == 10) begin
-              addr_4 = addr_1 + addr_3;
-            end else if(addr_2 == 25) begin
-              addr_4 = addr_1 - addr_3;
-            end else if(addr_2 == 13) begin
-              addr_4 = addr_1 / addr_3;
-            end else if(addr_2 == 21) begin
-              addr_4 = addr_1 * addr_3;
-            end
-            end
-        endcase
+          endcase
     end
     
 end
